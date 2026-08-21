a8468d094b19d4d508e6feb11de79278c653c474
Thomas Gonçalves <thomasgoncalves.dev@gmail.com>
2026-08-21 14:36:49 -0300

    chore: add initial setup spring

---
+++ backend/src/main/java/com/thomas/whatsrecovery/WhatsrecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.whatsrecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsrecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsrecoveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thomas/whatsrecovery/WhatsrecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.whatsrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsrecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
